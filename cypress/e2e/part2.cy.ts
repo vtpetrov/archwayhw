0717c52e45b33c7993bf756ff22b5eb454554367
vtpetrov <veselin.t.petrov@gmail.com>
2024-06-15 02:54:01 +0300

    Part 2 - verified 3 main buttons. - Verify 'Cookie Settings' Functionality - done TODO: - Verify 'Deny All Cookies' Functionality

--- cypress/e2e/part2.cy.ts
+++ cypress/e2e/part2.cy.ts
@@ -2,15 +2,15 @@ import * as domain from "node:domain";
 
 describe('Part 2 - Verify Cookie Settings Popup - approach 1 via home page', {testIsolation: true}, () => {
 
-    const homepageUrl = 'https://archway.finance';
+    const homepageUrl: string = 'https://archway.finance';
     const EXPECTED_ARCHWAY_COOKIE_COUNT: number = 8;
-    const archwayCookieDomain = '.archway.finance';
+    const archwayCookieDomain: string = '.archway.finance';
 
     //locators:
-    const ACCEPT_BTN_LOCATOR = 'div.cookie__buttons-grid.small > a[fs-cc = "allow"][href = "#"]'
-    const DENY_BTN_LOCATOR = 'div.cookie__buttons-grid.small > a[fs-cc = "deny"][href = "#"]'
-    const COOKIE_SETTINGS_BTN_LOCATOR = 'a[fs-cc = "submit"] > div[fs-cc = "open-preferences"]'
-    const CHAT_BTN_LOCATOR = 'div.intercom-lightweight-app-launcher.intercom-launcher[role = "button"]'
+    const ACCEPT_BTN_LOCATOR: string = 'div.cookie__buttons-grid.small > a[fs-cc = "allow"][href = "#"]'
+    const DENY_BTN_LOCATOR: string = 'div.cookie__buttons-grid.small > a[fs-cc = "deny"][href = "#"]'
+    const COOKIE_SETTINGS_BTN_LOCATOR: string = 'a[fs-cc = "submit"] > div[fs-cc = "open-preferences"]'
+    const CHAT_BTN_LOCATOR: string = 'div.intercom-lightweight-app-launcher.intercom-launcher[role = "button"]'
 
     beforeEach('open base url', () => {
         cy.visit(homepageUrl)
@@ -82,7 +82,8 @@ describe('Part 2 - Verify Cookie Settings Popup - approach 1 via home page', {te
         chainerToUse: "equal", valueToChain: ""
     }
 
-    it.only(`Clicking "Accept All Cookies" button stores all ${EXPECTED_ARCHWAY_COOKIE_COUNT} cookies in App storage`, () => {
+    it(`Clicking "Accept All Cookies" button stores all ${EXPECTED_ARCHWAY_COOKIE_COUNT} cookies in App storage`, () => {
+        cy.log("**Verify 'Accept All Cookies' Functionality**")
         // wait for "chat" button to appear before checking the cookies as they get fully stored only after the last API response which makes chat window to appear:
         cy.get(CHAT_BTN_LOCATOR, {timeout: 25000, log: true}).should('be.visible')
 
@@ -152,6 +153,53 @@ describe('Part 2 - Verify Cookie Settings Popup - approach 1 via home page', {te
             .its('value').should(expected_intercom_session_w06r82km_Cookie.chainerToUse
             , expected_intercom_session_w06r82km_Cookie.valueToChain)
 
+
+    })
+
+    const COOKIE_SETTINGS_TITLE_LOCATOR: string = 'div.ck-modal:not(.small) div[class*="heading-24px"]';
+    const STRICTLY_N_RADIO_LOCATOR: string = 'div.radio-wrapper.cookies.is--not-allowed.w-clearfix';
+    const MARKETING_RADIO_LOCATOR: string = 'div.w-checkbox-input:has(~ span[for="fs__marketing-2"])';
+    const MARKETING_RADIO_TEXT_LOCATOR: string = 'div.w-checkbox-input ~ span[for="fs__marketing-2"]';
+    const PERSONALIZATION_RADIO_LOCATOR: string = 'div.w-checkbox-input:has(~ span[for="fs__personalization-2"])';
+    const PERSONALIZATION_RADIO_TEXT_LOCATOR: string = 'div.w-checkbox-input ~ span[for="fs__personalization-2"]';
+    const ANALYTICS_RADIO_LOCATOR: string = 'div.w-checkbox-input:has(~ span[for="fs__analytics-2"])';
+    const ANALYTICS_RADIO_TEXT_LOCATOR: string = 'div.w-checkbox-input ~ span[for="fs__analytics-2"]';
+    const SAVE_SETTINGS_BTN_LOCATOR: string = 'div.ck-button__txt:contains("Save Settings")';
+
+
+    it('Verify \'Cookie Settings\' Functionality', () => {
+        cy.log("**Verify 'Cookie Settings' Functionality**")
+        cy.get(COOKIE_SETTINGS_BTN_LOCATOR).click()
+
+        // validate all cookie settings dialogue elements:
+        // Title:
+        cy.get(COOKIE_SETTINGS_TITLE_LOCATOR).should('be.visible')
+        cy.get(COOKIE_SETTINGS_TITLE_LOCATOR).should('have.text', 'Cookie Settings')
+
+        // radio buttons (Strictly necessary, Marketing, Personalization, Analytics):
+        cy.get(STRICTLY_N_RADIO_LOCATOR).should('be.visible')
+        cy.get(STRICTLY_N_RADIO_LOCATOR).find('.radio-label').should('have.text', 'Strictly Necessary (Always Active)')
+
+        cy.get(MARKETING_RADIO_LOCATOR).should('be.visible')
+        cy.get(MARKETING_RADIO_TEXT_LOCATOR).should('have.text', 'Marketing')
+
+        cy.get(PERSONALIZATION_RADIO_LOCATOR).should('be.visible')
+        cy.get(PERSONALIZATION_RADIO_TEXT_LOCATOR).should('have.text', 'Personalization')
+
+        cy.get(ANALYTICS_RADIO_LOCATOR).should('be.visible')
+        cy.get(ANALYTICS_RADIO_TEXT_LOCATOR).should('have.text', 'Analytics')
+
+        // Save settings button:
+        cy.get(SAVE_SETTINGS_BTN_LOCATOR).should('be.visible')
+        cy.get(SAVE_SETTINGS_BTN_LOCATOR).should('have.text', 'Save Settings')
+
+
+        // click "Marketing" radio button and assert changes
+        cy.get(MARKETING_RADIO_LOCATOR).click()
+        cy.get(MARKETING_RADIO_LOCATOR).should('have.class', 'w--redirected-checked')
+
+        // click Save settings
+        cy.get(SAVE_SETTINGS_BTN_LOCATOR).click()
 
     })
 })
